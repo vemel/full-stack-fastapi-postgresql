615f345f45c806aab18829ca6a27ddd78fcca3ed
Vlad Emelianov <volshebnyi@gmail.com>
2023-07-10 09:51:51 +0300

    Update config

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,5 +3,6 @@
 # Exit in case of error
 set -e
 
+# poetry run mypy \{\{cookiecutter.project_slug\}\}/backend
 poetry run isort \{\{cookiecutter.project_slug\}\}/backend
 poetry run black \{\{cookiecutter.project_slug\}\}/backend
--- scripts/generate_cookiecutter_config.py
+++ scripts/generate_cookiecutter_config.py
@@ -1,20 +1,22 @@
 import json
-from collections import OrderedDict
-import oyaml as yaml
 from pathlib import Path
-cookie_path = Path('./cookiecutter.json')
-out_path = Path('./{{cookiecutter.project_slug}}/cookiecutter-config-file.yml')
+from typing import Dict, Any
 
-with open(cookie_path) as f:
-    cookie_config = json.load(f)
-config_out = OrderedDict()
+import yaml
+
+
+cookie_path = Path("./cookiecutter.json")
+out_path = Path("./{{cookiecutter.project_slug}}/cookiecutter-config-file.yml")
+
+cookie_config: Dict[str, Any] = json.load(cookie_path.open())
+config_out: Dict[str, Any] = {}
 
 for key, value in cookie_config.items():
-    if key.startswith('_'):
+    if key.startswith("_"):
         config_out[key] = value
     else:
-        config_out[key] = '{{ cookiecutter.' + key + ' }}'
-config_out['_template'] = './'
+        config_out[key] = "{{ cookiecutter." + key + " }}"
+config_out["_template"] = "./"
 
-with open(out_path, 'w') as out_f:
-    out_f.write(yaml.dump({'default_context': config_out}, line_break=None, width=200))
+data = yaml.dump({"default_context": config_out}, line_break=None, width=200, sort_keys=False)
+out_path.write_text(data)
