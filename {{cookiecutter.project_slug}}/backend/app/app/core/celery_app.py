ead63674274f442704013b2c7393e622efdb9a7d
Vlad Emelianov <volshebnyi@gmail.com>
2023-07-13 11:21:11 +0300

    Fix celery warning

--- {{cookiecutter.project_slug}}/backend/app/app/core/celery_app.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/celery_app.py
@@ -2,4 +2,5 @@ from celery import Celery
 
 celery_app = Celery("worker", broker="amqp://guest@queue//")
 
+celery_app.conf.broker_connection_retry_on_startup = True
 celery_app.conf.task_routes = {"app.worker.test_celery": "main-queue"}
